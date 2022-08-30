3456aff644866341297fd155def80e9f2f6a69d7
kakaru1331 <kakaru1331@gmail.com>
2022-08-30 10:47:49 +0900

    http request 인증 설정

---
+++ src/main/java/me/kakaru/springobservabilitydemo/filter/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package me.kakaru.springobservabilitydemo.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeRequests()
+                    .antMatchers("/actuator/prometheus").authenticated()
+                    .anyRequest().permitAll().and()
+                .formLogin()
+                .and()
+                .httpBasic(withDefaults())
+        ;
+
+        return httpSecurity.build();
+    }
+}
